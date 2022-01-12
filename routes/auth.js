e1145b950d27f8824c0dcf86eff9554f2380dfa2
HeloiseFerla <ferla.heloise@gmail.com>
2022-01-12 14:25:29 +0100

    Remove dead code

--- routes/auth.js
+++ routes/auth.js
@@ -6,21 +6,6 @@ const jwt = require('jsonwebtoken');
 const { jwtSecret } = require('../conf');
 require('../passport-strategies');
 
-// router.post('/signup', async (req, res) => {
-//   try {
-//     const formData = req.body;
-//     formData.password = bcrypt.hashSync(formData.password, jwt_rounds);
-//     const [sqlRes] = await db.query(`INSERT INTO user SET ?`, formData);
-//     delete formData.password;
-//     formData.id = sqlRes.insertId;
-//     const token = jwt.sign(formData, jwt_secret);
-//     res.status(201).json({ user: formData, token });
-//   } catch (e) {
-//     console.log(e);
-//     res.status(500).json(e);
-//   }
-// });
-
 router.post('/login', passport.authenticate('local'), (req, res) => {
   const token = jwt.sign(req.user, jwtSecret);
   res.status(200).json({ user: req.user, token });
