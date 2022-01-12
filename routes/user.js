598a247fab0f1e5689d605feb23d982fd39dba81
BBriset <briset.b@gmail.com>
2022-01-12 10:29:31 +0100

    Adding User routes

---
+++ routes/user.js
@@ -0,0 +1,24 @@
+const userRouter = require('express').Router();
+const { db } = require('../conf');
+
+userRouter.get('/', async (req, res) => {
+  try {
+    const sql = 'SELECT * FROM User ';
+    const [user] = await db.query(sql);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+userRouter.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const sql = 'SELECT * FROM User where id=?';
+    const [user] = await db.query(sql, [id]);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+module.exports = userRouter;
