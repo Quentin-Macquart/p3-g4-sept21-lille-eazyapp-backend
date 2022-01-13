36ae2c57a98065b5b09670bf2bc2380082332bdd
HeloiseFerla <ferla.heloise@gmail.com>
2022-01-13 14:02:09 +0100

    Fix

--- routes/meeting.js
+++ routes/meeting.js
@@ -25,18 +25,6 @@ meetingRouter.get('/:id/participants', async (req, res) => {
   }
 });
 
-meetingRouter.get('/', async (req, res) => {
-  try {
-    const { id, slot } = req.query;
-    const sql =
-      'SELECT Meeting.id, Meeting.meetingRoomId as roomId, Meeting.beginning as slot, User.firstname as userFirstname, User.lastname as userLastname, User.picture as userPicture, MeetingRoom.picture as roomPicture FROM Meeting JOIN User ON Meeting.userId = User.id JOIN MeetingRoom ON Meeting.meetingRoomId = MeetingRoom.id WHERE meetingRoomId = ? AND beginning = ?';
-    const [meeting] = await db.query(sql, [id, slot]);
-    res.status(200).json(meeting);
-  } catch (err) {
-    res.status(400).send(err);
-  }
-});
-
 meetingRouter.post('/', async (req, res) => {
   const { beginning, userId, meetingRoomId } = req.body;
   const [resSql] = await db.query(
