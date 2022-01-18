5792f411bd8ef15ea47a4d07ba3c9f47a2eae65b
BBriset <briset.b@gmail.com>
2022-01-18 20:18:03 +0100

    Adding token gestion on user route

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,10 @@ const jwt = require('jsonwebtoken');
 const { jwtSecret } = require('../conf');
 require('../passport-strategies');
 
+router.post('/login/token', passport.authenticate('jwt'), (req, res) => {
+  const token = jwt.sign(req.user, jwtSecret);
+  res.status(200).json({ user: req.user, token });
+});
 router.post('/login', passport.authenticate('local'), (req, res) => {
   const token = jwt.sign(req.user, jwtSecret);
   res.status(200).json({ user: req.user, token });
--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,7 @@
 const userRouter = require('express').Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const { jwtSecret } = require('../conf');
 const { db } = require('../conf');
 
 userRouter.get('/', async (req, res) => {
@@ -10,23 +13,27 @@ userRouter.get('/', async (req, res) => {
     res.status(400).send(err);
   }
 });
-userRouter.get('/:id', async (req, res) => {
+userRouter.get('/:id', passport.authenticate('jwt'), async (req, res) => {
   try {
     const { id } = req.params;
     const sql = 'SELECT * FROM User where id=?';
-    const [user] = await db.query(sql, [id]);
-    res.status(200).json(user[0]);
+    await db.query(sql, [id]);
+    const user = { ...req.user };
+    const token = jwt.sign(user, jwtSecret);
+    res.status(200).json({ user, token });
   } catch (err) {
     res.status(400).send(err);
   }
 });
-userRouter.put('/:id', async (req, res) => {
+userRouter.put('/:id', passport.authenticate('jwt'), async (req, res) => {
   try {
     const { id } = req.params;
     const { amount } = req.body;
     const sql = 'UPDATE User SET amount=? WHERE id=?';
-    const [newAmount] = await db.query(sql, [amount, id]);
-    res.status(200).json(newAmount);
+    await db.query(sql, [amount, id]);
+    const user = { ...req.user, amount };
+    const token = jwt.sign(user, jwtSecret);
+    res.status(200).json({ user, token });
   } catch (err) {
     res.status(400).send(err);
   }
