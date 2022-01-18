3715728330f67ba943bf4dfbdeb493c8ff539d3c
HeloiseFerla <ferla.heloise@gmail.com>
2022-01-18 09:42:05 +0100

    Add meetingReset route to reset the meetings in the db

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const foodRouter = require('./food');
 const meetingRouter = require('./meeting');
 const authRouter = require('./auth');
 const userRouter = require('./user');
+const meetingResetRouter = require('./meetingReset');
 
 const setupRoutes = (app) => {
   app.use('/meetingRoom', meetingRoomRouter);
@@ -10,6 +11,7 @@ const setupRoutes = (app) => {
   app.use('/meeting', meetingRouter);
   app.use('/auth', authRouter);
   app.use('/user', userRouter);
+  app.use('/meetingReset', meetingResetRouter);
 };
 
 module.exports = {
