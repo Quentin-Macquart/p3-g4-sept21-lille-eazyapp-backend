e0be3ae3d772d4f0c9d892844b6f90d169bcfcf2
Quentin Macquart <macquart.quentin@gmail.com>
2022-01-18 15:41:02 +0100

    Update user Route to update direcly data in the front

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,8 @@
 const userRouter = require('express').Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 const { db } = require('../conf');
+const { jwtSecret } = require('../conf');
 
 userRouter.get('/', async (req, res) => {
   try {
@@ -31,49 +34,72 @@ userRouter.get('/:id/foodProfile', async (req, res) => {
     res.status(400).send(err);
   }
 });
-userRouter.post('/:id/foodProfile', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const {
-      eggFree,
-      glutenFree,
-      gmoFree,
-      nutFree,
-      sugarFree,
-      cornFree,
-      dairyFree,
-      soyFree,
-      transFatsFree,
-      vegan,
-      shellfishFree,
-      porkFree,
-      vegetarian,
-      fridayFish,
-      onDiet,
-    } = req.body;
-    const sql =
-      'UPDATE User SET eggFree=?, glutenFree=?, gmoFree=?, nutFree=?, sugarFree=?, cornFree=?, dairyFree=?, soyFree=?, transFatsFree=?, vegan=?, shellfishFree=?, porkFree=?, vegetarian=?, fridayFish=?, onDiet=? WHERE id=?';
-    const userFoodProfile = await db.query(sql, [
-      eggFree,
-      glutenFree,
-      gmoFree,
-      nutFree,
-      sugarFree,
-      cornFree,
-      dairyFree,
-      soyFree,
-      transFatsFree,
-      vegan,
-      shellfishFree,
-      porkFree,
-      vegetarian,
-      fridayFish,
-      onDiet,
-      id,
-    ]);
-    res.status(200).json(userFoodProfile);
-  } catch (err) {
-    res.status(400).send(err);
+userRouter.put(
+  '/:id/foodProfile',
+  passport.authenticate('jwt'),
+  async (req, res) => {
+    try {
+      const { id } = req.params;
+      const {
+        eggFree,
+        glutenFree,
+        gmoFree,
+        nutFree,
+        sugarFree,
+        cornFree,
+        dairyFree,
+        soyFree,
+        transFatsFree,
+        vegan,
+        shellfishFree,
+        porkFree,
+        vegetarian,
+        fridayFish,
+        onDiet,
+      } = req.body;
+      const sql =
+        'UPDATE User SET eggFree=?, glutenFree=?, gmoFree=?, nutFree=?, sugarFree=?, cornFree=?, dairyFree=?, soyFree=?, transFatsFree=?, vegan=?, shellfishFree=?, porkFree=?, vegetarian=?, fridayFish=?, onDiet=? WHERE id=?';
+      await db.query(sql, [
+        eggFree,
+        glutenFree,
+        gmoFree,
+        nutFree,
+        sugarFree,
+        cornFree,
+        dairyFree,
+        soyFree,
+        transFatsFree,
+        vegan,
+        shellfishFree,
+        porkFree,
+        vegetarian,
+        fridayFish,
+        onDiet,
+        id,
+      ]);
+      const user = {
+        ...req.user,
+        eggFree,
+        glutenFree,
+        gmoFree,
+        nutFree,
+        sugarFree,
+        cornFree,
+        dairyFree,
+        soyFree,
+        transFatsFree,
+        vegan,
+        shellfishFree,
+        porkFree,
+        vegetarian,
+        fridayFish,
+        onDiet,
+      };
+      const token = jwt.sign(user, jwtSecret);
+      res.status(200).json({ user, token });
+    } catch (err) {
+      res.status(400).send(err);
+    }
   }
-});
+);
 module.exports = userRouter;
