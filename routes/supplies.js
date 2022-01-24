1e6b9bcc37276f5875effb42d21a813eac098c9d
Othmane Khiter <othmane.khiter@gmail.com>
2022-01-24 18:16:38 +0100

    Added a first try of post routes and fix minor evils on user.js

--- routes/supplies.js
+++ routes/supplies.js
@@ -3,12 +3,29 @@ const { db } = require('../conf');
 
 suppliesRouter.get('/', async (req, res) => {
   try {
-    const sql = 'SELECT name, description, picture FROM SupplyItem ';
+    const sql = 'SELECT id, name, description, picture FROM SupplyItem ';
     const [supplyItem] = await db.query(sql);
     res.status(200).json(supplyItem);
   } catch (err) {
     res.status(400).send(err);
   }
+});
+
+suppliesRouter.post('/', async (req, res) => {
+  const { name, price, picture, quantity, orderTime, statut } = req.body;
+  const [resSql] = await db.query(
+    `INSERT INTO SupplyItem (name, price, picture) VALUES (?,?,?)`,
+    [name, price, picture],
+
+    `INSERT INTO Supplies (quantity) VALUES (?)`,
+    [quantity],
+
+    `INSERT INTO cartSupplies (orderTime, statut) VALUES (?, ?)`,
+    [orderTime, statut]
+  );
+  const newData = { ...req.body, id: resSql.insertId };
+
+  res.status(201).send(newData);
 });
 
 module.exports = suppliesRouter;
