1796e5566396b24be329d2aefa874d662f39d022
Othmane Khiter <othmane.khiter@gmail.com>
2022-01-26 11:45:00 +0100

    Updated supplies routes

--- routes/supplies.js
+++ routes/supplies.js
@@ -11,21 +11,25 @@ suppliesRouter.get('/', async (req, res) => {
   }
 });
 
-suppliesRouter.post('/', async (req, res) => {
-  const { name, price, picture, quantity, orderTime, statut } = req.body;
-  const [resSql] = await db.query(
-    `INSERT INTO SupplyItem (name, price, picture) VALUES (?,?,?)`,
-    [name, price, picture],
+suppliesRouter.post('/:id/cartSupplies', async (req, res) => {
+  try {
+    const items = req.body;
 
-    `INSERT INTO Supplies (quantity) VALUES (?)`,
-    [quantity],
+    const { id } = req.params;
+    const sql = 'INSERT INTO cartSupplies (userId) VALUES (?)';
+    const [cartSupplies] = await db.query(sql, [id]);
 
-    `INSERT INTO cartSupplies (orderTime, statut) VALUES (?, ?)`,
-    [orderTime, statut]
-  );
-  const newData = { ...req.body, id: resSql.insertId };
-
-  res.status(201).send(newData);
+    items.forEach((item) => {
+      item.push(cartSupplies.insertId);
+    });
+    console.log(items);
+    const sql2 =
+      'INSERT INTO Supplies ( quantity, SupplyItem_id, cartSupplies_id) VALUES ?';
+    const supplies = await db.query(sql2, [items[0]]);
+    res.status(200).json(cartSupplies, supplies);
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
 module.exports = suppliesRouter;
