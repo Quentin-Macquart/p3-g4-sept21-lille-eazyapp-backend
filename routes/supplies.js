bd797401d715a9cb731c50ae107bc30b0ff79037
Quentin Macquart <macquart.quentin@gmail.com>
2022-01-31 11:59:29 +0100

    Fix supplies name tables

--- routes/supplies.js
+++ routes/supplies.js
@@ -39,10 +39,10 @@ suppliesRouter.get('/:id/myOrder', async (req, res) => {
   try {
     const { id } = req.params;
     const sqlOrder =
-      'SELECT SupplyItem.name, SupplyItem.picture FROM SupplyItem INNER JOIN Supplies ON SupplyItemId = SupplyItem.id INNER JOIN CartSupplies ON CartSupplies.id = Supplies.cartSupplies_id INNER JOIN User ON User.id = CartSupplies.userId WHERE User.id= ?';
+      'SELECT SupplyItem.name, SupplyItem.picture FROM SupplyItem INNER JOIN Supplies ON supplyItemId = SupplyItem.id INNER JOIN CartSupplies ON CartSupplies.id = Supplies.cartSuppliesId INNER JOIN User ON User.id = CartSupplies.userId WHERE User.id= ?';
     const [orderRecap] = await db.query(sqlOrder, [id]);
     const sqlQuantity =
-      'SELECT Supplies.quantity FROM Supplies INNER JOIN CartSupplies ON CartSupplies.id = Supplies.cartSupplies_id INNER JOIN User ON User.id = CartSupplies.userId WHERE User.id = ? ';
+      'SELECT Supplies.quantity FROM Supplies INNER JOIN CartSupplies ON CartSupplies.id = Supplies.cartSuppliesId INNER JOIN User ON User.id = CartSupplies.userId WHERE User.id = ? ';
     const [quantityRecap] = await db.query(sqlQuantity, [id]);
     res.status(200).json({ orderRecap, quantityRecap });
   } catch (err) {
